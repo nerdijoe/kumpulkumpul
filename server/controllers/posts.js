6ac4b168caeda096d8e3b86636ae7a3f86170d76
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-12 09:57:50 +0700

    user can rsvp to the event

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 const Post = require('../models/posts')
 const methods = {}
 
@@ -65,6 +66,54 @@ methods.deleteById = function(req, res) {
                 res.json(record)
             }
         })
+}
+
+
+methods.addRsvp = function(req, res) {
+  var user_id = req.decoded._id;
+  console.log("** add RSVP, user_id=", user_id)
+
+  Post.findById( req.params.id, (error, post) => {
+    if(error) resn.json({error})
+    else {
+      if(post){
+        // check if user has already rsvp
+          // if yes do not add to rsvp
+          // if no, add user
+
+        // var user_id_obj = mongoose.Types.ObjectId(req.decoded._id);
+        // console.log(post.rsvp);
+        // var array = ["1","2,","3"];
+        // var elementPos = array.map(function(x) {return x; }).indexOf("3");
+        // var elementPos = post.rsvp.map(function(x) {return x; }).indexOf(user_id);
+        // console.log("___ elementPos=",elementPos)
+
+        var is_found = false;
+        post.rsvp.map( (id) => {
+          if(id==user_id) {
+            console.log("here")
+            is_found = true;
+          }
+        })
+
+        if(!is_found) {
+          post.rsvp.push(user_id);
+
+          post.save( (err, post) => {
+            if(error) resn.json({error})
+            res.json(post)
+          })
+        }
+        else {
+          var message = {
+            message: 'User has already RSVP to this event.'
+          }
+          res.json(message)
+        }
+
+      }
+    }
+  })
 }
 
 
